2ef701aa621da9f047aa6ba29a66db94ae83fd7a
Vanzh <vanzhula.pavlo@gmail.com>
2018-03-20 23:37:18 +0200

    5. Learn = if/else
    // Опускаем рыбу на 100 м.

--- src/Fish.java
+++ src/Fish.java
@@ -4,15 +4,25 @@ public class Fish extends Pet {
 
     public int dive(int howDeep) {
         currentDepth = currentDepth + howDeep;
-        System.out.println("Ныряю на глубину "
-                + howDeep + " метров");
-        System.out.println("Я на глубине "
-                + currentDepth + " метров ниже уровня моря");
+        if (currentDepth > 100) {
+            System.out.println("Я маленькая рыбка " +
+                    " и я не могу плавать глубже 100 метров");
+            currentDepth = currentDepth - howDeep;
+        }else{
+            System.out.println("Погружаюсь ещё на " + howDeep +
+                                                     " метров");
+            System.out.println("Я на глубине  " + currentDepth +
+                                                     " метров");
+        }
+//        System.out.println("Ныряю на глубину "
+//                + howDeep + " метров");
+//        System.out.println("Я на глубине "
+//                + currentDepth + " метров ниже уровня моря");
         return currentDepth;
     }
     @Override
         public String say(String something) {
-        return "Ты чё, не знаеш - рыбы не умеют разговаривать!";
+        return "Ты чё, не знаеш - рыбы не умеют разговаривать?";
     }
 
 }
--- src/FishMaster.java
+++ src/FishMaster.java
@@ -1,7 +1,9 @@
 public class FishMaster {
     public static void main(String[] args) {
         Fish myFish = new Fish();
+//   Опускаем рыбу на 100 м
         myFish.dive(2);
+        myFish.dive(97);
         myFish.dive(3);
         myFish.sleep();
 
